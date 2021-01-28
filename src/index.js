f6295af4500b82a540de8e223e3e8312a13e4661
caryaharper <caryaharper@gmail.com>
2021-01-27 18:31:59 -0600

    app functionaliy built out

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './app';
+import './index.css';
+
+ReactDOM.render(<Game name='name'/>, document.querySelector('#root'));
+  
